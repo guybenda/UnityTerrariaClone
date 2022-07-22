017dd8063a4fe26019b0ad25b5de27ad0b5a7112
guybenda <guybenda@users.noreply.github.com>
2022-07-22 19:52:42 +0300

    Movement tweaks

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,11 +7,11 @@ public static class PlayerConsts
     public static readonly float PLAYER_HEIGHT = 2.79f;
     public static readonly float PLAYER_WIDTH = 1.79f;
 
-    public static readonly float MAX_SPEED_REGULAR = 4f;
+    public static readonly float MAX_SPEED_REGULAR = 11f;
     public static readonly float ACCEL_FORCE_REGULAR = 1f;
 
-    public static readonly float JUMP_SPEED_REGULAR = 10f;
-    public static readonly float JUMP_TIME_REGULAR = 0.5f;
+    public static readonly float JUMP_SPEED_REGULAR = 17.2875f;
+    public static readonly float JUMP_TIME_REGULAR = 0.267f;
 
     public static readonly float SPEED_BRAKE_REGULAR = 6f;
 }
@@ -25,6 +25,9 @@ public class PlayerScript : MonoBehaviour
     public float BrakeSpeed = PlayerConsts.SPEED_BRAKE_REGULAR;
     public bool Grounded = false;
 
+    private bool jump = false;
+    private float movement = 0f;
+    private bool lastPressedA = false;
     private float currentJumpTimer = 0f;
 
     Camera cam;
@@ -44,6 +47,44 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         UpdateCameraPosition();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            lastPressedA = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            lastPressedA = false;
+        }
+
+        if (lastPressedA)
+        {
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                movement = -PlayerConsts.MAX_SPEED_REGULAR;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                movement = PlayerConsts.MAX_SPEED_REGULAR;
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                movement = PlayerConsts.MAX_SPEED_REGULAR;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                movement = -PlayerConsts.MAX_SPEED_REGULAR;
+            }
+        }
     }
 
     void FixedUpdate()
@@ -62,16 +103,43 @@ public class PlayerScript : MonoBehaviour
     {
         Grounded = CheckGrounded();
 
-        if (Input.GetKey(KeyCode.D))
+        /*if (Input.GetKey(KeyCode.D))
         {
             rb.AddRelativeForce(new Vector2(Acceleration, 0f), ForceMode2D.Impulse);
         }
-        else
-        if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A))
         {
             rb.AddRelativeForce(new Vector2(-Acceleration, 0f), ForceMode2D.Impulse);
+        }*/
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            var v = rb.velocity;
+            rb.velocity = Vector2.SmoothDamp(rb.velocity, new Vector2(0f, PlayerConsts.MAX_SPEED_REGULAR), ref v, 0.5f, PlayerConsts.MAX_SPEED_REGULAR);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            var v = rb.velocity;
+            rb.velocity = Vector2.SmoothDamp(rb.velocity, new Vector2(0f, -PlayerConsts.MAX_SPEED_REGULAR), ref v, 0.5f, PlayerConsts.MAX_SPEED_REGULAR);
         }
 
+        if (Grounded && jump)
+        {
+            rb.AddRelativeForce(new(0f, PlayerConsts.JUMP_SPEED_REGULAR), ForceMode2D.Impulse);
+            currentJumpTimer = PlayerConsts.JUMP_TIME_REGULAR;
+        }
+        else if (!Grounded && Input.GetKey(KeyCode.Space) && currentJumpTimer > 0f)
+        {
+            rb.AddRelativeForce(-Physics2D.gravity, ForceMode2D.Force);
+        }
+        else if (!Grounded && !Input.GetKey(KeyCode.Space) && currentJumpTimer > 0f)
+        {
+            currentJumpTimer = 0f;
+        }
+
+        jump = false;
+        /*
+        //TODO FIX THIS SHIT
         if (Input.GetKeyUp(KeyCode.Space))
         {
             currentJumpTimer = 0f;
@@ -91,7 +159,7 @@ public class PlayerScript : MonoBehaviour
             {
                 rb.AddRelativeForce(-Physics2D.gravity, ForceMode2D.Force);
             }
-        }
+        }*/
 
         if (rb.velocity.x > PlayerConsts.MAX_SPEED_REGULAR)
         {
@@ -120,5 +188,17 @@ public class PlayerScript : MonoBehaviour
         var hit = Physics2D.OverlapArea(point1, point2, LAYER_MASK);
 
         return hit != null;
+    }
+
+    void ClampHorizontalVelocity()
+    {
+        if (Grounded)
+        {
+
+        }
+        else
+        {
+
+        }
     }
 }
