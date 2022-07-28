2edc05171f9c78895f1dfa1545d8091100d058db
guybenda <email@example.com>
2022-07-28 21:11:12 +0300

    Inventory slots

--- Assets/Scripts/Game/Inventory.cs
+++ Assets/Scripts/Game/Inventory.cs
@@ -2,6 +2,10 @@
 {
     public class Inventory
     {
+        public readonly InventoryItem[] items;
+
+        public readonly int hotbarSlots = 10;
+        public readonly int inventoryRows = 4;
 
     }
 }
